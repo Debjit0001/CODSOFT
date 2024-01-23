989ebd3aca22dc848fcda3a579e39eae782b6c88
Debjit Mahato <debjitm234@gmail.com>
2024-01-23 21:41:08 +0530

    updated

--- calculator.py
+++ calculator.py
@@ -4,36 +4,44 @@ def calculator():
     while True:
         os.system('cls' if os.name == 'nt' else 'clear')
         print('\n\t~~:Calculator:~~\n')
-        # getting input from the user
+
+        # Getting input from the user for the first number
         num1 = input("Enter first number: ")
-        if not isinstance(float(num1), (int, float)):
+        try:
+            num1 = float(num1)
+        except ValueError:
             print('\nPlease enter a valid number')
             input('Press enter to continue...')
             continue
 
         print('\nChoose from: (+, -, *, /, %)')
-        operator = input("enter operator: ")
+        operator = input("Enter operator: ")
 
-        num2 = input("\nEnter second number: ")
-        
-        # checking for possible errors
-        if num2 == 0:
-            raise ZeroDivisionError("Cannot divide by zero!")
-        if not isinstance(float(num2), (int, float)):
-            print('\nPlease enter a valid number!')
+        # Performing the calculation based on the operator
+        if operator in ('+', '-', '*', '/', '%'):
+            num2 = input("\nEnter second number: ")
+            try:
+                num2 = float(num2)
+            except ValueError:
+                print('\nPlease enter a valid number!')
+                input('Press enter to continue...')
+                continue
+
+            print('---------------------------')
+
+            # Checking for division by zero
+            if num2 == 0 and operator == '/':
+                print("Cannot divide by zero!")
+                input('Press enter to continue...')
+                continue
+            
+            print(f'Result = {eval(str(num1) + operator + str(num2))}')
+        else:
+            print("\nInvalid operator")
             input('Press enter to continue...')
             continue
-        
-        print('---------------------------')
-        
-        # performing the calculation based on the operator
-        if operator in ('+', '-', '*', '/', '%'):
-            print(f'Result = {eval(num1 + operator + num2)}')
-        else:
-            print("Invalid operator")
-            continue
 
-        # asking if the user wants to continue
+        # Asking if the user wants to continue
         choice = input("\nDo you want to perform another calculation? (y/n): ")
         if choice.lower() != "y":
             print('Exiting the program. Goodbye! \n')
